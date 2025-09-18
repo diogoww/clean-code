0f9ee3eabc57b98db9df6fe411b29ea576cdc678
Diogo Varaschin <diogovaraschin@outlook.com>
2025-09-18 14:27:33 -0300

    feat: implement song_register_view and constructor with song_register_constructor

--- projeto 1/src/main/process_handler.py
+++ projeto 1/src/main/process_handler.py
@@ -1,11 +1,12 @@
 from src.view.first_view import introduction_page
+from .constructor.song_register_constructor import song_register_process
 
 def start() -> None:
     while True:
         
         command = introduction_page()
 
-        if command == '1': print("inserindo musica")
+        if command == '1': song_register_process
         elif command == '2': print("criando playlist")
         elif command == '5': exit()
         else: print("\n comando nao encontrado, tente novamente! \n\n")
