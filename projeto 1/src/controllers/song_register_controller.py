2444d1847673b7e93613cd12ee90df0d792e0c30
Diogo Varaschin <diogovaraschin@outlook.com>
2025-09-18 15:24:00 -0300

    feat: implement song register validations

--- projeto 1/src/controllers/song_register_controller.py
+++ projeto 1/src/controllers/song_register_controller.py
@@ -1,19 +1,40 @@
 class SongRegisterController:
     def insert(self, new_song_informations: dict) -> dict:
-        self.__verify_songs_infos()
-        self.__verify_if_song_already_registered()
-        self.__insert_song()
-        self.__format_response()
+        try:
+            self.__verify_songs_infos(new_song_informations)
+            self.__verify_if_song_already_registered(new_song_informations)
+            self.__insert_song(new_song_informations)
+            self.__format_response(new_song_informations)
+        except Exception as exception:
+            self.__format_error_response(exception)
+
+    def __verify_songs_infos(self, new_song_informations: dict) -> None:
+        if len(new_song_informations["title"]) > 100:
+            raise Exception("Digite um título com menos de 100 caracteres.")
+        
+        year = int(new_song_informations["year"])
+        if year >= 2026:
+            raise Exception("Ano inválido")
+
+    def __verify_if_song_already_registered(self, new_song_informations: dict) -> None:
+        #models
         pass
 
-    def __verify_if_song_already_registered(new_song_informations: dict) -> None:
+    def __insert_song(self, new_song_informations: dict) -> None:
+        #models
         pass
 
-    def __verify_songs_infos(new_song_informations: dict) -> None:
-        pass
-
-    def __insert_song() -> None:
-        pass
-
-    def __format_response() -> None:
-        pass
+    def __format_response(self, new_song_informations: dict) -> dict:
+        return{
+            "success": True,
+            "count": 1,
+            "attributes": {
+                "title": new_song_informations["title"]
+            }
+        }
+    
+    def __format_error_response(self, err: Exception) -> dict:
+        return{
+                "success": False,
+                "error": str(err)
+            }
